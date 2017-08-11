c17b3f8add9d85483f882986a860c9cfc170d6b2
Kota Uchino <k.uchino@everyleaf.com>
2017-08-11 23:01:58 +0900

    Add .tmux.conf

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-DOTFILES=(.vimrc .zshrc)
+DOTFILES=(.tmux.conf .vimrc .zshrc)
 
 for file in ${DOTFILES[@]}
 do
   ln -fnsv $HOME/dotfiles/$file $HOME/$file
 done
+
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+  git clone git@github.com:tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+fi
 
 if [ ! -d ~/.zplug ]; then
   curl -sL zplug.sh/installer | zsh
