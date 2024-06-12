0b55d3426187eb0977154bdc70c0d47936f79ca3
Kota Uchino <uchinokota@gmail.com>
2024-06-12 14:18:06 +0900

    Add F12

--- keyboards/crkbd/rev4/standard/keymap.c
+++ keyboards/crkbd/rev4/standard/keymap.c
@@ -10,9 +10,9 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3_ex2(
   //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_LPRN,    KC_RPRN,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_MINS,
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, XXXXXXX,    XXXXXXX,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_MINS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_LBRC,    KC_RBRC,    KC_H,    KC_J,    KC_K,    KC_L, KC_COLN, KC_QUOT,
+      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G, XXXXXXX,     KC_F12,    KC_H,    KC_J,    KC_K,    KC_L, KC_COLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------'  `--------+--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
@@ -22,9 +22,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT_split_3x6_3_ex2(
   //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
-       KC_ESC, KC_EXLM,   KC_AT,  KC_EQL,  KC_GRV, KC_TILD, KC_LPRN,    KC_RPRN, XXXXXXX, KC_UNDS, XXXXXXX, XXXXXXX, KC_PLUS, KC_BSPC,
+       KC_ESC, KC_EXLM,   KC_AT,  KC_EQL,  KC_GRV, KC_TILD, XXXXXXX,    XXXXXXX, XXXXXXX, KC_UNDS, XXXXXXX, XXXXXXX, KC_PLUS, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-      KC_LCTL,   KC_AT, KC_ASTR,  KC_DLR, XXXXXXX, XXXXXXX, KC_LBRC,    KC_RBRC, KC_HASH, KC_LPRN, KC_RPRN, XXXXXXX, KC_SCLN, KC_QUOT,
+      KC_LCTL,   KC_AT, KC_ASTR,  KC_DLR, XXXXXXX, XXXXXXX, XXXXXXX,     KC_F12, KC_HASH, KC_LPRN, KC_RPRN, XXXXXXX, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------'  `--------+--------+--------+--------+--------+--------+--------|
       KC_LSFT, KC_LALT, XXXXXXX, KC_CIRC, KC_PIPE, KC_BSLS,                      KC_AMPR, KC_LBRC, KC_RBRC, KC_DOT,  XXXXXXX, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
